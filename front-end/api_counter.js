18d41af66508279eb1b068d490e553ad6f425aa6
mohrav723 <94524432+mohrav723@users.noreply.github.com>
2021-12-27 20:44:32 -0800

    Delete front-end directory

--- front-end/api_counter.js
+++
@@ -1,9 +0,0 @@
-updateVisitCount();
-function updateVisitCount() {
-    fetch('https://fn2qro7sr4.execute-api.us-west-1.amazonaws.com/Dev/method', {method: 'POST'})
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            document.getElementById('count').innerHTML = data;
-        })
-    }
